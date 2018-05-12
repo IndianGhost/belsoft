4975fa2ba3a405b86cf50b8131d2f6c0a1d1fd25
IndianGhost <achraf.bellaali@gmail.com>
2018-05-12 13:34:37 +0100

    First step on Front end DONE !

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', [
+    'as'    =>  'home',
+    'uses'  =>  'RouteController@home'
+]);
+
+Route::get('/about', [
+    'as'    =>  'about',
+    'uses'  =>  'RouteController@about'
+]);
+
+Route::get('/contact', [
+    'as'    =>  'contact',
+    'uses'  =>  'RouteController@contact'
+]);
